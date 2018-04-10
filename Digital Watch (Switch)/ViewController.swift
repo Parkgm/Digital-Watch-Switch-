7de85ed2088f553a381748c0ad0282c7c25d6994
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-04-10 15:33:17 +0900

    current time + switch

--- Digital Watch (Switch)/ViewController.swift
+++ Digital Watch (Switch)/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
+    var myTimer = Timer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        timerSwitch.setOn(false, animated: true)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        timeLabel.text = formatter.string(from: date)
+        //        timeLabel.text = String(describing: date)
+    }
+  
+   
+    @IBAction func timerStart(_ sender: UISwitch) {
+        if timerSwitch.isOn == true {
+            
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
 }
+    
+
 
